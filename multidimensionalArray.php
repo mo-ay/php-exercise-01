5ce07ddb4c3f5f1e035740f5f44b26ebaf608051
mo-ay <mostafaaanezan@gmail.com>
2020-10-09 03:16:45 +0300

    Completed with sorting

--- multidimensionalArray.php
+++ multidimensionalArray.php
@@ -2,10 +2,13 @@
 $multiDarray =[ "musicals" => ["Oklahoma", "The Music Man", "South Pacific "]
 ,"dramas" =>[ "Lawrence of Arabia", "To Kill a Mockingbird", "Casablanca "] ,"mysteries"
 =>["The Maltese Falcon",  "Rear Window",  "North by Northwest"]];
+rsort($multiDarray);
 foreach ($multiDarray as $key => $value) {
     echo strtoupper($key) . "<br>"; 
     foreach ($value as $sub_key => $sub_val){
         echo "----> $sub_key = $sub_val <br>";
     }
 }
+rsort($multiDarray);
+
 ?>
